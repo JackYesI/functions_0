1c5a8500baa609a28120d47c425f39dded4c385a
DEV <dev@step.rivne>
2023-08-14 16:13:56 +0300

    First lesson

---
+++ claswork_14_08_alone.py
@@ -0,0 +1,105 @@
+# 2 1)
+# def division(x, y):
+#     print(x / y)
+
+# 2 2) 
+# def division(x, y):
+#     try:
+#         print(x / y)
+#     except ZeroDivisionError:
+#         print('division of zero')
+
+# 1 
+# try: 
+#     num = int(input('Enter number: '))
+#     num1 = int(input("Enter number: "))
+#     print(num/num1)
+# except ZeroDivisionError:
+#     print('Division of Zero')
+
+# 2 1)
+# try: 
+#     num = int(input('Enter number: '))
+#     num1 = int(input("Enter number: "))
+#     division(num, num1)
+# except ZeroDivisionError:
+#     print('Division of Zero')
+
+# 2 2)
+# num = int(input('Enter number: '))
+# num1 = int(input("Enter number: "))
+# division(num, num1)
+
+# 3
+# try:
+#     str_ofNumber = float(input("Enter str: "))
+#     print(type(str_ofNumber))
+# except ValueError:
+#     print('ValueError')
+
+# 4 1)
+
+# def str_(x):
+#     x = float(x)
+#     print(type(x))
+
+# try:
+#     str_ofNumber = input("Enter str: ")
+#     str_(str_ofNumber)
+# except ValueError:
+#     print('ValueError')
+
+# 4 2)
+# def str_(x):
+#     try: 
+#         x = float(x)
+#         print(type(x))
+#     except ValueError:
+#         print('ValueError')
+
+
+# str_ofNumber = input("Enter str: ")
+# str_(str_ofNumber)
+
+# Second part
+# 1
+# try:
+#     number = int(input("Enter number: "))
+#     if number < 0:
+#         raise Exception
+#     print(number**2)
+# except Exception:
+#     print('number less than zero')
+
+# 2 1)
+# def pow_2(x):
+#     print(x**2)
+
+# try:
+#     number = int(input("Enter number: "))
+#     if number < 0:
+#         raise Exception
+#     pow_2(number)
+# except Exception:
+#     print('number less than zero')
+
+# 2 2)
+def pow_2(x):
+    try:
+        if x < 0:
+            raise Exception
+        print(x**2)
+    except Exception:
+        print('number less than zero')
+
+
+number = int(input("Enter number: "))
+pow_2(number)
+
+
+
+
+
+
+
+
